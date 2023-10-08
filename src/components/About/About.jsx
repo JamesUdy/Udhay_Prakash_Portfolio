ce8f4ad1ddce5fc1b4559306032ce0ecbb8d2f7a
JamesUdy <udayamvad@gmail.com>
2023-10-08 21:21:32 +0530

    refactor: extract about card

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,11 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import AboutCard from "./AboutCard";
 import {motion} from 'framer-motion';
 import { services } from "../../constants";
 import {styles} from '../../styles';
 import "./About.css";
 import {fadeIn, textVariant} from '../../utils/motion';
 import { Wrapper } from "../../hoc";
-
-const ServiceCard = ({index, title, icon}) => {
-    return (
-        <Tilt className="w-[200px]">
-            <motion.div variants={fadeIn("right", "spring", 0.5*index, 0.75)} className="w-full about-card-gradient p-[1px] rounded-[20px] shadow-card">
-            <div options={{
-                max: 45,
-                scale: 1,
-                speed: 450
-            }} className="about-card-bg rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-                <h3 className="font-bold hero-text1 text-[20px] text-center">{title}</h3>
-                <img src={icon} alt={title}  className="w-16 h-16 object-contain" />
-            </div>
-            </motion.div>
-        </Tilt>
-    )
-}
 
 const About = () => {
     return (
@@ -36,7 +19,7 @@ const About = () => {
             </motion.p>
             <div className="mt-20 flex flex-wrap gap-10 pb-10 justify-center">
                 {services.map((service,index) => (
-                    <ServiceCard key={service.title} index={index} {...service} />
+                    <AboutCard key={service.title} index={index} {...service} />
                 ))}
             </div>
         </>

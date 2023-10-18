306e88351c8f738a6698a4a43200a6ea7aec0c2c
JamesUdy <udayamvad@gmail.com>
2023-10-18 22:46:53 +0530

    ui: style download resume button

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -49,7 +49,7 @@ const Skills = () => {
                 </Marquee>
             </div> 
             <div className="my-10">
-                <button onClick={onButtonClick} className="rounded-lg p-3 lg:p-4 download"><span className="font-light text-md md:text-lg cursor-pointer">Download Resume</span> 👇🏼</button> 
+                <button onClick={onButtonClick} className="border-4 bg-transparent border-[#200737d9] shadow-lg shadow-[#200737d9] hover:shadow-xl hover:shadow-[#200737d9] ease-in duration-200 text-gray-300 rounded-xl px-4 py-2"><span className="font-light text-md md:text-lg cursor-pointer">Download Resume</span> 👇🏼</button> 
             </div>
         </div>
     )

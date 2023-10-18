2288f3546801dcc52eff373e8731008b5de8a34e
JamesUdy <udayamvad@gmail.com>
2023-10-18 22:32:45 +0530

    ui: text justify style in project comp

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
                 <h2 className={styles.sectionHeadText}>Projects.</h2>
             </>
             <div className="w-full flex">
-                <p className="mt-3 text-secondary text-[17px] max-w-3l md:max-w-6xl leading-[30px]">The projects below are instances of my work in the real world that highlight my experience and skills. References to source code repositories and real-time demonstrations are included with each project's brief description. It demonstrates my capacity to handle various technologies, handle complicated difficulties, and efficiently organize projects.</p>
+                <p className="mt-3 text-secondary text-[17px] text-justify max-w-3l md:max-w-6xl leading-[30px]">The projects below are instances of my work in the real world that highlight my experience and skills. References to source code repositories and real-time demonstrations are included with each project's brief description. It demonstrates my capacity to handle various technologies, handle complicated difficulties, and efficiently organize projects.</p>
             </div>
             <div className="mt-20 flex flex-wrap gap-7 pb-10  justify-start sm:justify-center">
                 {projects.slice(0, visibleProjects).map((project,index) => (

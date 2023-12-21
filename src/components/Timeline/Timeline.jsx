b7c2051180a53475460c5ae748b9da526d23eb70
JamesUdy <udayamvad@gmail.com>
2023-12-21 12:52:02 +0530

    copy: remove unwanted code

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,5 +1,4 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-import { color, motion } from "framer-motion"
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 import { styles } from "../../styles";
 import { timeline } from "../../constants";
